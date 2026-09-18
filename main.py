cd93cdfdd0d23b7e77796b15ab0d0c5c8549f18b
Napadpadakosalangsangan <salangsang.ca@obmontessori.edu.ph>
2026-09-18 21:34:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ from pyscript import document, display
 
 def order(e):
 
+# Prices
   burger = 5
   fries = 3
   soda = 1
 
-# How do I let my pyscript know if it's checked or not T-T
+# Detecting the checkboxes
   burger_ordered = float(document.getElementById("burger").checked)
   fries_ordered = float(document.getElementById("fries").checked)
   soda_ordered = float(document.getElementById("soda").checked)
 
+# Calculating (w/ Booleans returning 1s and 0s)
   subtotal = float(burger * burger_ordered + fries * fries_ordered + soda * soda_ordered)
 
+# 12% vat
   vat = subtotal * 0.12
 
+# After vat
   total = subtotal + vat
 
+#  Displaying the receipt
   document.getElementById("subtotal").innerText = f"${subtotal}"
   document.getElementById("vat").innerText = f"${vat}"
   document.getElementById("total").innerText = f"${total}"
